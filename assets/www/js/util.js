353f36c279b5ce8101ef26da6f2b95d9539a8a2b
Brien Coffield <bcoffield@ptc.com>
2012-01-10 11:01:51 -0600

    Finished xml to json converter. Might have to license the GPL version under a different license

--- assets/www/js/util.js
+++ assets/www/js/util.js
@@ -1,37 +1,57 @@
 com = {};
 com.eyekabob = {};
 com.eyekabob.util = {};
-
-// TODO: make child json objects first, then traverse xml children
+/**
+ * Converts xml DOM to json.
+ * Note: no attributes are converted.
+ * Result will contain only nodeNames and node.data where applicable.
+ * Also skips all whitespace.
+ */
 com.eyekabob.util.xmlToJson = function(xml, json) {
     if (json === undefined) {
+        // Should only happen on the first call.
         json = {};
     }
 
-    if (xml.data === undefined) {
-        var dataStructure = {};
-        if (json[xml.nodeName] === undefined) {
-            json[xml.nodeName] = dataStructure;
+    if (typeof(xml) === "string") {
+        // Typically you want to pass xml that has already been parsed.
+        // From an xmlhttpresponse, use xmlhttpresponse.responseXML instead
+        // of responseText to skip this branch.
+        xml = (new DOMParser()).parseFromString(xml, "text/xml");
+    }
+
+    var i = 0;
+    for (; i < xml.childNodes.length; i++) {
+        var childNode = xml.childNodes[i];
+        if (childNode.data !== undefined) {
+            if (childNode.data.trim() !== "") {
+                json.data = childNode.data;
+            }
+            continue;
         }
-        else if (typeof(json.length) === "undefined") {
-            var old = json[xml.nodeName];
-            json = [];
-            json.push(old);
-            json.push(dataStructure);
+
+        var dataStructure = {};
+        if (json[childNode.nodeName] === undefined) {
+            // Node has not been seen before. Pass empty object as json.
+            json[childNode.nodeName] = dataStructure;
+        }
+        else if (json[childNode.nodeName].length === undefined) {
+            // Node has been seen once before. Convert to array.
+            var old = json[childNode.nodeName];
+            json[childNode.nodeName] = [];
+            json[childNode.nodeName].push(old);
+            json[childNode.nodeName].push(dataStructure);
         }
         else {
-            json.push(dataStructure);
+            // Node has been seen at least twice now. Just push new object to array.
+            json[childNode.nodeName].push(dataStructure);
         }
 
-        var i = 0;
-        for (; i < xml.childNodes.length; i++) {
-            com.eyekabob.util.xmlToJson(xml.childNodes[i], dataStructure);
-        }
-    }
-    else if (xml.data.trim() !== "") {
-        json.data = xml.data;
+        // Mutate json recursively.
+        com.eyekabob.util.xmlToJson(childNode, dataStructure);
     }
 
+    // All mutations finished, return result.
     return json;
 };
 
