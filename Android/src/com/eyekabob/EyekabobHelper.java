d48852899627c953a0f664b47556f3f6935c6251
Brien Coffield <coffbr01@gmail.com>
2012-02-16 23:27:01 -0600

    helper with last.fm values

---
+++ Android/src/com/eyekabob/EyekabobHelper.java
@@ -0,0 +1,12 @@
+package com.eyekabob;
+
+public class EyekabobHelper {
+	public static class LastFM {
+		public static final String USER = "eyekabob";
+		public static final String PASS = "eyekabob";
+		public static final String API_KEY = "d9cd2f150cf10b139ab481e462272f3f";
+		public static final String SECRET = "3a0bdf658071649c7a2594aa63ec1062";
+		public static final String AUTH_TOKEN = "167550e2be8e2989ab56e63b03dd1db6";
+		public static final String SERVICE_URL = "http://ws.audioscrobbler.com/2.0/";
+	}
+}
--- Android/src/com/eyekabob/FindMusicActivity.java
+++ Android/src/com/eyekabob/FindMusicActivity.java
@@ -1,8 +1,16 @@
 package com.eyekabob;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class FindMusicActivity extends Activity {
@@ -13,6 +21,35 @@ public class FindMusicActivity extends Activity {
     }
 
     public void findByArtistHandler(View v) {
-    	Toast.makeText(v.getContext(), "pressed find by artist", 3000);
+        EditText findByArtist = (EditText)findViewById(R.id.findByArtistInput);
+        String artist = findByArtist.getText().toString();
+        URL url = null;
+		try {
+			url = new URL(EyekabobHelper.LastFM.SERVICE_URL + "?method=artist.getEvents&api_key=" + EyekabobHelper.LastFM.API_KEY + "&artist=" + artist);
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+
+        HttpURLConnection urlConnection = null;
+		try {
+			urlConnection = (HttpURLConnection) url.openConnection();
+            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+            String result = getStringResponse(in);
+            Toast.makeText(v.getContext(), result, 8000).show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+        finally {
+            urlConnection.disconnect();
+        }
+    }
+
+    public String getStringResponse(InputStream is) throws IOException {
+        StringBuffer out = new StringBuffer();
+        byte[] b = new byte[4096];
+        for (int n; (n = is.read(b)) != -1;) {
+            out.append(new String(b, 0, n));
+        }
+        return out.toString();
     }
 }
