503b92958531327e7b50fe02c5c454bcd61daf7c
Brien Coffield <coffbr01@gmail.com>
2012-02-17 00:25:52 -0600

    some async artist rework

--- Android/src/com/eyekabob/FindMusicActivity.java
+++ Android/src/com/eyekabob/FindMusicActivity.java
@@ -4,7 +4,6 @@ import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 import android.app.Activity;
@@ -24,29 +23,24 @@ public class FindMusicActivity extends Activity {
     public void findByArtistHandler(View v) {
         EditText findByArtist = (EditText)findViewById(R.id.findByArtistInput);
         String artist = findByArtist.getText().toString();
-        URL url = null;
-		try {
-			url = new URL(EyekabobHelper.LastFM.SERVICE_URL + "?method=artist.getEvents&api_key=" + EyekabobHelper.LastFM.API_KEY + "&artist=" + artist);
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		}
-
-		new ArtistTask().execute(url);
-
+        String url = EyekabobHelper.LastFM.SERVICE_URL + "?method=artist.getEvents&api_key=" + EyekabobHelper.LastFM.API_KEY + "&artist=" + artist;
+        new ArtistTask().execute(url);
     }
 
-    private class ArtistTask extends AsyncTask<URL, Void, String> {
-    	protected String doInBackground(URL... urls) {
+    // Handles the asynchronous request, away from the UI thread.
+    private class ArtistTask extends AsyncTask<String, Void, String> {
+    	protected String doInBackground(String... urls) {
     		return doRequest(urls[0]);
     	}
 
     	protected void onPostExecute(String result) {
     		Toast.makeText(getApplicationContext(), result, 50000).show();
     	}
-        public String doRequest(URL url) {
+        public String doRequest(String urlStr) {
         	HttpURLConnection urlConnection = null;
         	StringBuffer out = null;
     		try {
+    			URL url = new URL(urlStr);
     			urlConnection = (HttpURLConnection) url.openConnection();
                 InputStream is = new BufferedInputStream(urlConnection.getInputStream());
                 out = new StringBuffer();
@@ -54,7 +48,8 @@ public class FindMusicActivity extends Activity {
                 for (int n; (n = is.read(b)) != -1;) {
                     out.append(new String(b, 0, n));
                 }
-    		} catch (IOException e) {
+    		}
+    		catch (IOException e) {
     			e.printStackTrace();
     		}
             finally {
