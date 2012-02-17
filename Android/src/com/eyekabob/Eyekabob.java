e7dd2e5f6e7b49e71eacd7be2c53b027279f6d4f
Brien Coffield <coffbr01@gmail.com>
2012-02-16 21:38:18 -0600

    smaller find music button

--- Android/src/com/eyekabob/Eyekabob.java
+++ Android/src/com/eyekabob/Eyekabob.java
@@ -2,11 +2,18 @@ package com.eyekabob;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class Eyekabob extends Activity {
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+    public void findMusicHandler(View v) {
+        Toast.makeText(v.getContext(), "WOOT", 3000).show();
     }
 }
