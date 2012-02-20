2c6825c2832b7162e36472554d79c2db748701cf
Brien Coffield <bcoffield@ptc.com>
2012-02-19 20:03:23 -0600

    rework how the request is sent

--- Android/src/com/eyekabob/EyekabobHelper.java
+++ Android/src/com/eyekabob/EyekabobHelper.java
@@ -1,5 +1,16 @@
 package com.eyekabob;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Map;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
 public class EyekabobHelper {
 	public static class LastFM {
 		public static final String USER = "eyekabob";
@@ -8,5 +19,62 @@ public class EyekabobHelper {
 		public static final String SECRET = "3a0bdf658071649c7a2594aa63ec1062";
 		public static final String AUTH_TOKEN = "167550e2be8e2989ab56e63b03dd1db6";
 		public static final String SERVICE_URL = "http://ws.audioscrobbler.com/2.0/";
+
+		public static void makeRequest(Context context, String method, Map<String, String> params) {
+			String url = SERVICE_URL;
+			url += "?method=" + method;
+			url += "&api_key=" + API_KEY;
+			for (String param : params.keySet()) {
+				url += "&" + param + "=" + params.get(param);
+			}
+
+			new EyekabobHelper.RequestTask(context).execute(url);
+		}
+
 	}
+
+    // Handles the asynchronous request, away from the UI thread.
+    public static class RequestTask extends AsyncTask<String, Void, String> {
+    	private Context context;
+    	private Toast searching;
+    	public RequestTask(Context context) {
+    		this.context = context;
+    	    searching = Toast.makeText(context, R.string.searching, Integer.MAX_VALUE);
+    	}
+    	public Context getContext() {
+    		return context;
+    	}
+    	protected void onPreExecute() {
+    		searching.show();
+    	}
+    	protected String doInBackground(String... urls) {
+    		return doRequest(urls[0]);
+    	}
+    	protected void onPostExecute(String result) {
+    		searching.cancel();
+    		Toast.makeText(context, result, 50000).show();
+    	}
+        public String doRequest(String urlStr) {
+        	HttpURLConnection urlConnection = null;
+        	StringBuffer out = null;
+    		try {
+    			URL url = new URL(urlStr);
+    			urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream is = new BufferedInputStream(urlConnection.getInputStream());
+                out = new StringBuffer();
+                byte[] b = new byte[4096];
+                for (int n; (n = is.read(b)) != -1;) {
+                    out.append(new String(b, 0, n));
+                }
+    		}
+    		catch (IOException e) {
+    			e.printStackTrace();
+    		}
+            finally {
+                urlConnection.disconnect();
+            }
+
+            return out.toString();
+        }
+    }
 }
--- Android/src/com/eyekabob/FindMusicActivity.java
+++ Android/src/com/eyekabob/FindMusicActivity.java
@@ -1,17 +1,12 @@
 package com.eyekabob;
 
-import java.io.BufferedInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 import android.app.Activity;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.Toast;
 
 public class FindMusicActivity extends Activity {
     @Override
@@ -23,40 +18,9 @@ public class FindMusicActivity extends Activity {
     public void findByArtistHandler(View v) {
         EditText findByArtist = (EditText)findViewById(R.id.findByArtistInput);
         String artist = findByArtist.getText().toString();
-        String url = EyekabobHelper.LastFM.SERVICE_URL + "?method=artist.getEvents&api_key=" + EyekabobHelper.LastFM.API_KEY + "&artist=" + artist;
-        new ArtistTask().execute(url);
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("artist", artist);
+        EyekabobHelper.LastFM.makeRequest(getApplicationContext(), "artist.getEvents", params);
     }
 
-    // Handles the asynchronous request, away from the UI thread.
-    private class ArtistTask extends AsyncTask<String, Void, String> {
-    	protected String doInBackground(String... urls) {
-    		return doRequest(urls[0]);
-    	}
-
-    	protected void onPostExecute(String result) {
-    		Toast.makeText(getApplicationContext(), result, 50000).show();
-    	}
-        public String doRequest(String urlStr) {
-        	HttpURLConnection urlConnection = null;
-        	StringBuffer out = null;
-    		try {
-    			URL url = new URL(urlStr);
-    			urlConnection = (HttpURLConnection) url.openConnection();
-                InputStream is = new BufferedInputStream(urlConnection.getInputStream());
-                out = new StringBuffer();
-                byte[] b = new byte[4096];
-                for (int n; (n = is.read(b)) != -1;) {
-                    out.append(new String(b, 0, n));
-                }
-    		}
-    		catch (IOException e) {
-    			e.printStackTrace();
-    		}
-            finally {
-                urlConnection.disconnect();
-            }
-
-            return out.toString();
-        }
-    }
 }
