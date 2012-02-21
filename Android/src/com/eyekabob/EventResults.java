83a47a16f10257837ed5c111863b100365eff629
Brien Coffield <bcoffield@ptc.com>
2012-02-20 21:21:22 -0600

    events from artists

--- Android/src/com/eyekabob/EventResults.java
+++ Android/src/com/eyekabob/EventResults.java
@@ -1,8 +1,6 @@
 package com.eyekabob;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -17,30 +15,15 @@ import android.app.AlertDialog;
 import android.app.AlertDialog.Builder;
 import android.app.Dialog;
 import android.app.ListActivity;
-import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 
-public class ArtistResults extends ListActivity {
+public class EventResults extends ListActivity {
 	private Dialog alertDialog;
 	ArrayAdapter<String> adapter;
-	private OnItemClickListener listItemListener = new OnItemClickListener() {
-		public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-			String artist = ((TextView) view).getText().toString();
-			Intent intent = new Intent(getApplicationContext(), EventResults.class);
-			Map<String, String> params = new HashMap<String, String>();
-			params.put("artist", artist);
-			intent.setData(EyekabobHelper.LastFM.getUri("artist.getEvents", params));
-			startActivity(intent);
-		}
-	};
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,7 +33,6 @@ public class ArtistResults extends ListActivity {
         new RequestTask().execute(uri.toString());
         ListView lv = getListView();
         lv.setTextFilterEnabled(true);
-        lv.setOnItemClickListener(listItemListener);
     }
 
     // TODO: use dialogfragment to show dialog
@@ -62,24 +44,44 @@ public class ArtistResults extends ListActivity {
 	    alertDialog.setOwnerActivity(this);
     }
     
-    protected void loadArtists(Document doc) {
-    	NodeList artists = doc.getElementsByTagName("artist");
+    protected void loadEvents(Document doc) {
+    	NodeList artists = doc.getElementsByTagName("event");
     	for (int i = 0; i < artists.getLength(); i++) {
+    		String title = "";
+    		String venue = "";
+    		String startDate = "";
+    		String startTime = "";
     		Node artistNode = artists.item(i);
     		NodeList artistChildren = artistNode.getChildNodes();
     		for (int j = 0; j < artistChildren.getLength(); j++) {
     		    Node artistChildNode = artistChildren.item(j);
-    		    if ("name".equals(artistChildNode.getNodeName())) {
-    		    	adapter.add(artistChildNode.getTextContent());
+    		    String nodeName = artistChildNode.getNodeName();
+    		    if ("title".equals(nodeName)) {
+    		    	title = artistChildNode.getTextContent();
+    		    }
+    		    else if ("venue".equals(nodeName)) {
+    		    	NodeList venueChildren = artistChildNode.getChildNodes();
+    		    	for (int k = 0; k < venueChildren.getLength(); k++) {
+    		    		if ("name".equals(venueChildren.item(k).getNodeName())) {
+    		    			venue = venueChildren.item(k).getTextContent();
+    		    		}
+    		    	}
+    		    }
+    		    else if ("startDate".equals(nodeName)) {
+    		    	startDate = artistChildNode.getTextContent();
+    		    }
+    		    else if ("startTime".equals(nodeName)) {
+    		    	startTime = artistChildNode.getTextContent();
     		    }
     		}
+		    adapter.add(title + "\n" + venue + "\n" + startDate + " " + startTime);
     	}
     }
 
     // Handles the asynchronous request, away from the UI thread.
     private class RequestTask extends AsyncTask<String, Void, Document> {
     	protected void onPreExecute() {
-    		ArtistResults.this.createDialog();
+    		EventResults.this.createDialog();
     		alertDialog.show();
     	}
     	protected Document doInBackground(String... uris) {
@@ -87,7 +89,7 @@ public class ArtistResults extends ListActivity {
     	}
     	protected void onPostExecute(Document result) {
     		alertDialog.dismiss();
-    		ArtistResults.this.loadArtists(result);
+    		EventResults.this.loadEvents(result);
     	}
         public Document doRequest(String uri) {
         	Document result = null;
