bb2413c559199c02a8ccbc2de60dba4230a04f72
Brien Coffield <bcoffield@ptc.com>
2012-04-07 01:18:46 -0500

    fill out example artist rendering for Phil

--- Android/src/com/eyekabob/ArtistInfo.java
+++ Android/src/com/eyekabob/ArtistInfo.java
@@ -1,21 +1,78 @@
 package com.eyekabob;
 
-import android.app.Activity;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.webkit.WebView;
+import android.widget.TextView;
 
 import com.eyekabob.models.Artist;
+import com.eyekabob.util.DocumentTask;
+import com.eyekabob.util.EyekabobHelper;
+import com.eyekabob.util.NiceNodeList;
 
-public class ArtistInfo extends Activity {
+public class ArtistInfo extends EyekabobActivity {
+	private Artist artist;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.artist_info);
-		Artist artist = (Artist)getIntent().getExtras().get("artist");
+		WebView summaryWebView = (WebView)findViewById(R.id.artistSummary);
+		summaryWebView.setBackgroundColor(0x00000000);
+		artist = (Artist)getIntent().getExtras().get("artist");
 		Log.d(getClass().getName(), "Artist name is: " + artist.getName());
 
-		// TODO: request info based on artist name
+		Map<String, String> params = new HashMap<String, String>();
+		params.put("mbid", artist.getMbid());
+		Uri artistInfoUri = EyekabobHelper.LastFM.getUri("artist.getInfo", params);
+		new ArtistRequestTask().execute(artistInfoUri.toString());
 	}
 
+	protected void handleArtistResponse(Document result) {
+		NiceNodeList artistNodeList = new NiceNodeList(result.getElementsByTagName("artist").item(0).getChildNodes());
+		Map<String, Node> artistNodes = artistNodeList.get("name", "mbid", "url", "bio");
+		artist.setName(artistNodes.get("name").getTextContent());
+		artist.setMbid(artistNodes.get("mbid").getTextContent());
+		artist.setUrl(artistNodes.get("url").getTextContent());
+
+		NiceNodeList bioNodeList = new NiceNodeList(artistNodes.get("bio").getChildNodes());
+		Map<String, Node> bioNodes = bioNodeList.get("summary", "content");
+		artist.setSummary(bioNodes.get("summary").getTextContent());
+		artist.setContent(bioNodes.get("content").getTextContent());
+
+		render();
+	}
+
+	private void render() {
+		// TODO: Phil to put rendering code here.
+
+		// Example:
+		// ------------------------------------------------------------------------------------
+		TextView headerTextView = (TextView)findViewById(R.id.artistHeader);
+		headerTextView.setText(artist.getName());
+
+		WebView summaryWebView = (WebView)findViewById(R.id.artistSummary);
+		String summaryHtml = "<div style='color:white'>" + artist.getSummary() + "</div>";
+		summaryWebView.loadData(summaryHtml, "text/html", "UTF8");
+		// ------------------------------------------------------------------------------------
+	}
+
+    // Handles the asynchronous request, away from the UI thread.
+    private class ArtistRequestTask extends DocumentTask {
+    	protected void onPreExecute() {
+    		ArtistInfo.this.createDialog(R.string.loading);
+    		ArtistInfo.this.showDialog();
+    	}
+    	protected void onPostExecute(Document result) {
+    		ArtistInfo.this.dismissDialog();
+    		ArtistInfo.this.handleArtistResponse(result);
+    	}
+    }
 }
