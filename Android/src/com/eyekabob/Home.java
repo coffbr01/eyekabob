4b13fbd449c2866da09550e21a82d1a1f898c142
Brien Coffield <bcoffield@ptc.com>
2012-04-11 21:15:20 -0500

    added facebook sdk and hooked up login to the checkin button

--- Android/src/com/eyekabob/Home.java
+++ Android/src/com/eyekabob/Home.java
@@ -4,9 +4,15 @@
  */
 package com.eyekabob;
 
+import com.eyekabob.util.EyekabobHelper;
+import com.eyekabob.util.facebook.DialogError;
+import com.eyekabob.util.facebook.Facebook;
+import com.eyekabob.util.facebook.FacebookError;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class Home extends EyekabobActivity {
 
@@ -20,18 +26,47 @@ public class Home extends EyekabobActivity {
         Intent findMusicIntent = new Intent(this, FindMusic.class);
         startActivity(findMusicIntent);
     }
+
     public void homePicsHandler(View v) {
     }
+
     public void homeFriendsHandler(View v) {
     }
+
     public void homeCheckinHandler(View v) {
+    	Facebook facebook = EyekabobHelper.Facebook.getInstance();
+    	if (!facebook.isSessionValid()) {
+    		facebook.authorize(this, new Facebook.DialogListener(){
+				public void onComplete(Bundle values) {
+					// TODO: checkin!
+				    Toast.makeText(getApplicationContext(), "Facebook login successful", Toast.LENGTH_SHORT).show();
+				}
+				public void onFacebookError(FacebookError e) {
+					// TODO Auto-generated method stub
+				}
+				public void onError(DialogError e) {
+					// TODO Auto-generated method stub
+				}
+				public void onCancel() {
+					// TODO Auto-generated method stub
+				}
+    		});
+    		return;
+    	}
+
+    	// TODO: checkin!
+    	Toast.makeText(this, "You're already signed in!", Toast.LENGTH_SHORT).show();
     }
+
     public void homeSettingsHandler(View v) {
     }
+
     public void homeBandHandler(View v) {
     }
+
     public void homeForumHandler(View v) {
     }
+
     public void homeAdHandler(View v) {
     }
 }
