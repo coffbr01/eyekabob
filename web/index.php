b1727655ada0ad17fe03ed56242d6daad00d4ee3
Brien Coffield <bcoffield@ptc.com>
2012-05-06 21:52:06 -0500

    web service for adding artist
    bcoffield.dyndns.org/eyekabob/web?artist=aNewArtist

--- web/index.php
+++ web/index.php
@@ -6,17 +6,23 @@
     $json = array(
         "results" => array()
     );
+
+    $con = connect_to_db();
+
     if ($_GET["method"] === "artist") {
-        $con = connect_to_db();
         $query = mysql_query("SELECT * FROM artist WHERE name='" . $_GET["name"] . "%'");
         while ($row = mysql_fetch_array($query)) {
             array_push($json["results"], $row["name"]);
         }
-        mysql_close($con);
+    }
+    else if ($_POST["method"] === "artist") {
+        mysql_query("INSERT INTO artist (name, mbid, url, bio) VALUES ('" . $_POST["name"] . "', '" . $_POST["mbid"] . "', '" . $_POST["url"] ."', '" . $_POST["bio"] .  "')");
     }
     else {
-        $json["error"] = "TODO: make nice messaging for invalid service request";
+        $json["error"] = "Unrecognized request";
     }
+
+    mysql_close($con);
 ?>
 <?php
     echo json_encode($json);
