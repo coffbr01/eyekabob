5b04aaca12c926341ea8696bd9bc1a059a40ee9a
Brien Coffield <bcoffield@ptc.com>
2012-05-15 22:41:11 -0500

    some web service debugging to determine the request verb

--- web/index.php
+++ web/index.php
@@ -10,12 +10,14 @@
     $con = connect_to_db();
 
     if ($_GET["method"] === "artist") {
+        array_push($json["verb"], "GET");
         $query = mysql_query("SELECT * FROM artist WHERE name='" . $_GET["name"] . "%'");
         while ($row = mysql_fetch_array($query)) {
             array_push($json["results"], $row["name"]);
         }
     }
     elseif ($_POST["method"] === "artist") {
+        array_push($json["verb"], "POST");
         mysql_query("INSERT INTO artist (genre, name, url, bio) VALUES ('" . $_POST["genre"] . "', '" . $_POST["name"] . "', '" . $_POST["url"] ."', '" . $_POST["bio"] .  "')");
         array_push($json["results"], $_POST["name"]);
     }
