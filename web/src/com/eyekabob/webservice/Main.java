f4e67f9d1d488cbe35c1959184b3cdf47868f91f
Brien Coffield <bcoffield@ptc.com>
2012-05-16 21:05:01 -0500

    deploy war to tomcat and initial json object response

--- web/src/com/eyekabob/webservice/Main.java
+++ web/src/com/eyekabob/webservice/Main.java
@@ -2,11 +2,20 @@ package com.eyekabob.webservice;
 
 import javax.servlet.http.HttpServletRequest;
 
+import net.sf.json.JSONException;
 import net.sf.json.JSONObject;
 
 public class Main {
     public static JSONObject processRequest(HttpServletRequest request) {
-        JSONObject result = new JSONObject();
+        JSONObject result = null;
+
+        try {
+            result = new JSONObject();
+        }
+        catch (JSONException e) {
+            e.printStackTrace();
+        }
+
         return result;
     }
 }
