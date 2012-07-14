c7eaaf82101bb33d64bcfcbd5b1dce47bce31988
Brien Coffield <coffbr01@gmail.com>
2012-07-14 11:04:40 -0500

    Simple first web service returns {"response":"Hello, world!"}
    Using jersey rest apis, create a web service that returns mimetype
    application/json with some static content.
    
    Also move license.txt to the root directory

---
+++ web/src/main/java/com/eyekabob/EyekabobApplication.java
@@ -0,0 +1,21 @@
+package com.eyekabob;
+
+import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * © Copyright 2012 Brien Coffield
+ * All rights reserved
+ * <p/>
+ * This file is subject to the terms and conditions defined in
+ * file 'LICENSE.txt', which is part of this source code package.
+ */
+public class EyekabobApplication extends Application {
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> classes = new HashSet<Class<?>>();
+        classes.add(Main.class);
+        return classes;
+    }
+}
--- web/src/main/java/com/eyekabob/Main.java
+++ web/src/main/java/com/eyekabob/Main.java
@@ -1,14 +1,26 @@
 package com.eyekabob;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
-@Path("/rest/main")
+/**
+ * © Copyright 2012 Brien Coffield
+ * All rights reserved
+ * <p/>
+ * This file is subject to the terms and conditions defined in
+ * file 'LICENSE.txt', which is part of this source code package.
+ */
+@Path("/main")
 public class Main {
     @GET
-    @Produces("text/plain")
-    public String helloWorldMethod() {
-        return "Hello, world!";
+    @Produces("application/json")
+    public String helloWorld() throws JSONException {
+        JSONObject response = new JSONObject();
+        response.put("response", "Hello, world!");
+        return response.toString();
     }
 }
