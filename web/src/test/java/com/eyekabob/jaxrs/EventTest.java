5780f11db6d3f4193a1497f796fcb226e9a37d8f
Brien Coffield <coffbr01@gmail.com>
2012-07-22 11:07:10 -0500

    More event web service tests and a root android clean target

--- web/src/test/java/com/eyekabob/jaxrs/EventTest.java
+++ web/src/test/java/com/eyekabob/jaxrs/EventTest.java
@@ -1,11 +1,13 @@
 package com.eyekabob.jaxrs;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class EventTest {
     private Event eventService;
@@ -37,5 +39,29 @@ public class EventTest {
         result = eventService.getResponse("", "");
         jsonResult = new JSONObject(result);
         assertTrue(jsonResult.has("error"));
+    }
+
+    @Test
+    public void testGetResponseByID() throws Exception {
+        String result = eventService.getResponseByID("anID");
+        JSONObject jsonResult = new JSONObject(result);
+        assertTrue(jsonResult.has("event"));
+
+        JSONObject event = jsonResult.getJSONObject("event");
+        assertTrue(event.has("id"));
+
+        assertEquals("anID", event.get("id"));
+    }
+
+    @Test
+    public void testGetResponseBySearchTerm() throws Exception {
+        String result = eventService.getResponseBySearchTerm("aSearchTerm");
+        JSONObject jsonResult = new JSONObject(result);
+        assertTrue(jsonResult.has("search"));
+        assertEquals("aSearchTerm", jsonResult.get("search"));
+
+        assertTrue(jsonResult.has("events"));
+        JSONArray resultEvents = jsonResult.getJSONArray("events");
+        assertEquals(0, resultEvents.length());
     }
 }
